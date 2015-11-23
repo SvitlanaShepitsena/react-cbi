ea6232b0527e783c499bf4d15e9ec16e1da9ea7b
SvitlanaShepitsena <chicagobusinessintelligence1@gmail.com>
2015-11-23 14:13:58 -0600

    add files

---
+++ app/components/Logo.js
@@ -0,0 +1,13 @@
+'use strict'
+import React from 'react';
+
+export default class Logo extends React.Component {
+    render() {
+        return (
+            <div>
+
+
+            </div>
+        );
+    }
+}
